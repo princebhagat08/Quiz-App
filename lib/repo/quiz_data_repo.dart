598bad253d02b6c1e4fdf71c50f6469dae1fc953
Prince Kumar <princebhagat12336@gmail.com>
2025-01-20 15:32:44 +0530

    video

--- lib/repo/quiz_data_repo.dart
+++ lib/repo/quiz_data_repo.dart
@@ -5,6 +5,7 @@ import '../models/quiz_data_model.dart';
 class QuizDataRepo{
   final _apiService  = NetworkApiServices() ;
 
+  // Fetching Quiz Data
   Future<QuizDataModel> quizData() async{
     dynamic response = await _apiService.getApi(Apis.quizDataUrl);
     return QuizDataModel.fromJson(response) ;
